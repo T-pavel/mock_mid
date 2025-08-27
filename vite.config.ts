8586ef35bac82b1448a4987e5153f2572a5d0af0
Pavel Tkachenko <tkachenko_ps@nlmk.com>
2025-08-27 20:16:38 +0300

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GH_PAGES_BASE || '/',
-})
+  base: process.env.GH_PAGES_BASE || "/mock_mid/",
+});
